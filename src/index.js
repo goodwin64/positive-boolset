055e51070f5e5c79a87d6c81387a33658590ea10
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-11-27 16:13:07 +0200

    via simple "for" loop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getPositiveKeysOfBoolset(boolset) {
-  // your solution here
-}
+    const result = [];
+    for (let index = 1, iterations = boolset.length; index < iterations; index += 2) {
+        if (index % 2 === 1 && boolset[index]) {
+            result.push(boolset[index - 1]);
+        }
+    }
+    return result;
+};
